6554ef08653400da7568f3f318e88e9a85d19311
laodeng <93652888+laodengcode@users.noreply.github.com>
2025-02-17 13:20:10 +0100

    add book

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const myLibrary = [];
 
-let harryPorter = new Book("Harry Porter", "JK", "this is an amazing magic book.this is an amazing magic book.this is an amazing magic book.");
+let harryPorter = new Book("Harry Porter", "JK", "this is an amazing magic book.this is an amazing magic book.this is an amazing magic book.", 0, 300, false);
 myLibrary.push(harryPorter);
 myLibrary.push(harryPorter);
 myLibrary.push(harryPorter);
 myLibrary.push(harryPorter);
 
-function Book(name, author, description) {
+function Book(name, author, description, id, pageNumber, isRead) {
     this.name = name;
     this.author = author;
     this.description = description;
+    this.id = id;
+    this.pageNumber = pageNumber;
+    this.isRead = isRead;
 }
 
-function addBookToLibrary(name, author, description) {
-    let book = new Book(name, author, description);
+Book.prototype.toggleReadStatus = function () {
+    this.isRead = !this.isRead;
+}
+
+function addBookToLibrary(name, author, description, pageNumber, isRead) {
+    let id = myLibrary.length === 0 ? 0 : myLibrary[myLibrary.length - 1].id + 1;
+    let book = new Book(name, author, description, id, pageNumber, isRead);
     myLibrary.push(book);
 }
 
@@ -36,5 +44,34 @@ function displayLibrary() {
         bookList.appendChild(bookItem);
     })
 }
+
+
+let addBookModal = document.getElementById('addBook');
+
+document.getElementById('openDialog').addEventListener('click', () => {
+    addBookModal.showModal();
+})
+
+document.getElementById('closeDialog').addEventListener('click', () => {
+    addBookModal.close();
+})
+
+document.getElementById('bookForm').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const author = document.getElementById('author').value;
+    const description = document.getElementById('description').value;
+    const pages = document.getElementById('pages').value;
+    const isRead = document.getElementById('isRead').checked;
+
+    console.log(name);
+    console.log(isRead);
+
+    addBookToLibrary(name, author, description, pages, isRead);
+    addBookModal.close();
+    event.target.reset();
+    displayLibrary();
+})
+
 
 displayLibrary();
